26b73bbbdbe1b831d22025a45b0e22c327965b47
illai <illai.zeevi@evinced.com>
2024-05-08 13:11:46 +0300

    Updates

--- src/pages/challenges/index.js
+++
@@ -1,4 +0,0 @@
-
-export { default as ChallengeOne } from "./challenge-one";
-export { default as ChallengeTwo } from "./challenge-two";
-export { default as ChallengeThree } from "./challenge-three";
